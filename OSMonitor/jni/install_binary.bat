885abe8de61640f1d6ba3bb7f7abf0c3d65be21e
eolwral <eolwral@gmail.com>
2014-04-13 05:34:38 +0800

    add native library for armabi-v7a

--- OSMonitor/jni/install_binary.bat
+++ OSMonitor/jni/install_binary.bat
@@ -1,5 +1,6 @@
 @echo Copy binary execute to assets..
 @move .\armeabi\osmcore .\armeabi\libosmcore.so
+@move .\armeabi-v7a\osmcore .\armeabi-v7a\libosmcore.so
 @move .\x86\osmcore .\x86\libosmcore.so
 @move .\mips\osmcore .\mips\libosmcore.so
 @echo Done..

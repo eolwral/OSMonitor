5959f36528df7b5e994420b4b7f6ebf7e171c170
eolwral <eolwral@gmail.com>
2014-08-10 22:34:12 +0800

    version 3.3.0.2

--- OSMonitor/jni/install_binary.sh
+++ OSMonitor/jni/install_binary.sh
@@ -4,6 +4,6 @@ mv ./armeabi/osmcore ../assets/osmcore_arm
 mv ./x86/osmcore ../assets/osmcore_x86
 mv ./mips/osmcore ../assets/osmcore_mips
 mv ./armeabi/osmcore_l ../assets/osmcore_arm_l
-mv ./x86/osmcore_l ../assets/osmcore_x86_l
-mv ./mips/osmcore_l ../assets/osmcore_mips_l
+# mv ./x86/osmcore_l ../assets/osmcore_x86_l
+# mv ./mips/osmcore_l ../assets/osmcore_mips_l
 echo Done..

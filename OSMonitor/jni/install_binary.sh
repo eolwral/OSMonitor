be5dd7614766f504cea8d1998e9a93183205e197
eolwral <eolwral@gmail.com>
2015-02-02 00:15:06 +0800

    fix install binary script for Linux/Mac

--- OSMonitor/jni/install_binary.sh
+++ OSMonitor/jni/install_binary.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo Copy binary execute to assets..
-mv ./armeabi/osmcore ../assets/osmcore_arm
-mv ./x86/osmcore ../assets/osmcore_x86
-mv ./mips/osmcore ../assets/osmcore_mips
-mv ./armeabi/osmcore_pie ../assets/osmcore_arm_pie
-mv ./x86/osmcore_pie ../assets/osmcore_x86_pie
-mv ./mips/osmcore_pie ../assets/osmcore_mips_pie
+mkdir assets
+mv libs/armeabi/osmcore assets/osmcore_arm
+mv libs/x86/osmcore assets/osmcore_x86
+mv libs/mips/osmcore assets/osmcore_mips
+mv libs/armeabi/osmcore_pie assets/osmcore_arm_pie
+mv libs/x86/osmcore_pie assets/osmcore_x86_pie
+mv libs/mips/osmcore_pie assets/osmcore_mips_pie
 echo Done..

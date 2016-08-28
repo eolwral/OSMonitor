eb59047a99e6c3f85e2506cbca4bed371ac57d25
eolwral <eolwral@gmail.com>
2016-08-28 19:54:58 +0800

    upgrade build tools and gradle version

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -2,5 +2,5 @@ APP_PLATFORM  := android-9
 APP_STL                 := gnustl_static
 APP_ABI                 := armeabi x86 mips
 
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_CPPFLAGS += -std=c++11
